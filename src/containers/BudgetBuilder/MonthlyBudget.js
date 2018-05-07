598f32bed712254085534fe6f3c3aa1260f549b8
loganrun <bigdog_41@hotmail.com>
2018-05-07 16:25:52 +0000

    red

--- src/containers/BudgetBuilder/MonthlyBudget.js
+++ src/containers/BudgetBuilder/MonthlyBudget.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import Grid from 'material-ui/Grid'
-//import Paper from 'material-ui/Paper'
-//import { income, expense } from '../../store.js'
 import Aux from '../../hoc/Aux';
 import BudetItem from '../Forms/BudgetItem'
 import ResponsiveDialog from '../Forms/DialogBox'
@@ -10,55 +8,30 @@ import ResponsiveDialog from '../Forms/DialogBox'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 //import ExpenseStream from '../../components/layouts/Main/ExpenseStream'
 //import IncomeStream from '../../components/layouts/Main/IncomeStream'
-//import BudgetMonth from '../../components/layouts/Main/BudgetMonth'
-//import ActualMonth from '../../components/layouts/Main/ActualMonth'
-//import IconButton from 'material-ui/IconButton';
-//import { FormControl, FormHelperText } from 'material-ui/Form';
-//import * as actionTypes from '../store/actions'
-//import {connect} from 'react-redux'
-//import TextField from 'material-ui/TextField';
-//import Table, { TableCell, TableHead, TableRow, TableFooter } from 'material-ui/Table';
-//import { withStyles } from 'material-ui/styles';
+
+import * as actionTypes from '../store/actions'
+import {connect} from 'react-redux'
+import {bindactionCreators} from 'redux'
 import IncomeTable from '../Forms/IncomeTable';
 import ExpenseTable from '../Forms/ExpenseTable';
-//import createMuiTheme from 'material-ui/styles'
-//import MenuItem from 'material-ui/Menu/MenuItem';
-//import Visibility from '@material-ui/icons/Visibility';
-//import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import './MonthlyBudget.css'
 
-//import LayoutBudget from '../../components/layouts/LayoutBudget'
-//import BudgetCategories from '../../components/Budget/BudgetCategories/Budgetcategories'
 
-
-// const muiTheme = createMuiTheme ({
-//     palette: {
-//         primary1Color: 'orange500',
-//         accentColor: 'deepOrange500',
-//     }
-//})
  
 class Monthlybudget extends Component{
     state = {
         data: [],
     }
     
-    
-    
-
-    
  render(){
-     const incomeStream = this.state.data.filter(incomeObject =>{
+     const incomeStream = this.props.budget.filter(incomeObject =>{
         return incomeObject.type === 'income'
     })
     
     
-     const expenseStream = this.state.data.filter(incomeObject =>{
+     const expenseStream = this.props.budget.filter(incomeObject =>{
         return incomeObject.type === 'expense'
     })
-    //  const expenseTotal = this.state.data.reduce(function (firstTotal,nextTotal) {
-    //      return firstTotal + nextTotal
-    // })
     
      return(
          <MuiThemeProvider>
@@ -127,10 +100,18 @@ class Monthlybudget extends Component{
     
  }   
 }
+function mapStateToProps(state){
+    budget: state.budget
+}
 
+function mapDispatchToProps = dispatch => {
+    return{
+        
+    }
+}
  
 
-export default Monthlybudget;
+export default connect(mapStateToProps, mapDispatchToProps)(Monthlybudget);
 
 
 
